29fe53b8052dbb7514f49a5cdb4aff3f65faad01
amitdavidi <amitdavidi12@gmail.com>
2023-01-09 09:44:23 +0200

    added Almost equals and get argument

--- ComplexNumber.java
+++ ComplexNumber.java
@@ -1,8 +1,11 @@
+import java.lang.Math;
+
 public class ComplexNumber {
 
+    /* Data members */
     private final double real_part;
     private final double imaginary_part;
-
+    private static final double EPSILON = 0.001;
 
     /* Constructor */
     public ComplexNumber(double realPart, double imaginaryPart) {
@@ -47,6 +50,24 @@ public class ComplexNumber {
                                (this.imaginary_part * other.real_part - this.real_part * other.imaginary_part) / divisor);
     }
 
+
+    public double getRadius() {
+        return Math.sqrt(real_part * real_part + imaginary_part * imaginary_part);
+      }
+
+
+    public double getArgument() {
+
+        return Math.atan2(imaginary_part, real_part);
+    }
+
+    public boolean almostEquals(ComplexNumber other) {
+        ComplexNumber difference = this.minus(other);
+        double radius = difference.getRadius();
+        return radius < EPSILON;
+    }
+
+      
     public static void main(String[] argumentss) {
         
     }
